98445a5b632c2d1424bf88538e950d475290d5e0
Oleksandr_Rudenko2 <oleksandr_rudenko2@epam.com>
2019-06-07 17:12:02 +0300

    Hotfix

--- modules/iam_roles/main.tf
+++ modules/iam_roles/main.tf
@@ -2,6 +2,6 @@ resource "aws_iam_role" " role" {
   name = "aws_iot_cloudwatchMetric"
   description = "IAM role for CloudWatch Metrics"
   tags = {
-    tag-key = "${var.name_prefix}-IAM-CloudWatchMetrics"
+    tag-key = "IAM-CloudWatchMetrics"
   }
 }
